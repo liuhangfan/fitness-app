5390dac9f11c90060b338fa9fde80bce924ebda0
Hangfan Liu <liuhangfan27@gmail.com>
2023-01-04 13:16:42 -0800

    fix img

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -1,25 +1,25 @@
 import React from 'react';
-import { Box, Stack, Typography } from '@mui/material';
+import { Box, Stack, Typography, Grid} from '@mui/material';
 
-import HeroBannerImage from '../assets/images/banner.png';
+import HeroBannerImage from '../assets/images/Fitness.svg';
 
 const HeroBanner = () => (
-  <Box sx={{ mt: { lg: '212px', xs: '70px' }, ml: { sm: '50px' } }} position="relative" p="20px">
-    <Typography color="#FF2625" fontWeight="600" fontSize="26px">Fitness Club</Typography>
-    <Typography fontWeight={700} sx={{ fontSize: { lg: '44px', xs: '40px' } }} mb="23px" mt="30px">
-      Sweat, Smile <br />
-      And Repeat
-    </Typography>
-    <Typography fontSize="22px" fontFamily="Alegreya" lineHeight="35px">
-      Check out the most effective exercises personalized to you
-    </Typography>
-    <Stack>
-      <a href="#exercises" style={{ marginTop: '45px', textDecoration: 'none', width: '200px', textAlign: 'center', background: '#FF2625', padding: '14px', fontSize: '22px', textTransform: 'none', color: 'white', borderRadius: '4px' }}>Explore Exercises</a>
-    </Stack>
-    <Typography fontWeight={600} color="#FF2625" sx={{ opacity: '0.1', display: { lg: 'block', xs: 'none' }, fontSize: '200px' }}>
-      Exercise
-    </Typography>
-    <img src={HeroBannerImage} alt="hero-banner" className="hero-banner-img" />
+  <Box sx={{ ml: { sm: '50px' }  ,flexWrap: 'wrap', display: 'flex', flexGrow:"0"}} position="relative" p="20px">
+        <Box sx={{mt: { lg: '180px', xs: '70px' }}}>
+        <Typography color="#FF2625" fontWeight="500" fontSize="80px">Fitness Club</Typography>
+        <Typography fontWeight={700} sx={{ fontSize: { lg: '44px', xs: '40px' } }} mb="23px" mt="30px">
+        Sweat, Smile And Repeat
+        </Typography>
+        <Typography fontSize="22px" fontFamily="Alegreya" lineHeight="35px">
+        Check out the most effective exercises personalized to you
+        </Typography>
+        <Stack>
+        <a href="#exercises" style={{ marginTop: '45px', textDecoration: 'none', width: '200px', textAlign: 'center', background: '#FF2625', padding: '14px', fontSize: '22px', textTransform: 'none', color: 'white', borderRadius: '4px' }}>Explore Exercises</a>
+        </Stack>
+        </Box>
+        {/* <Box alignItems="center" justify="center" ><img src={HeroBannerImage} alt="hero-banner"  position="relative"  width="" /></Box> */}
+        <Box component="img"  src={HeroBannerImage} alt="hero-banner" sx={{width:{xs:'100%', md:'55%', sm:"100%"}}}></Box>
+        
   </Box>
 );
 
